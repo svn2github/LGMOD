8fb295edcb906846ffed7b9d027ce1d68021fa24
mmm4m5m <mmm4m5m@6f01ea5f-641d-df11-ac65-003048919802>
2011-08-29 06:34:46 +0000

    version: 1.0.10 - looks ready and working
    git-svn-id: http://svn.openlgtv.org.ru@603 6f01ea5f-641d-df11-ac65-003048919802

--- lgmod_s7/create_img.sh
+++ lgmod_s7/create_img.sh
@@ -6,78 +6,61 @@
 LGMOD_VERSION="1.0.10"
 LGMOD_VERSION_EPK="30333"
 LGMOD_VERSION_ROOTFS="10010"
+ofile=lgmod_S7_$LGMOD_VERSION_ROOTFS
+oinit=mtd4_lg-init
+size=7340032
+
 mkepk_bin=../pack/mkepk
 mksquashfs_bin=../pack/mksquashfs
-
 dir=trunk/rootfs
-size=7340032
 cd ${0%/*}
-if [ ! -f "$mksquashfs_bin" ]
-then
-    echo "ERROR: mksquashfs not found."
-    exit 1
-elif [ ! -d "$dir" ]
-then
-    echo "ERROR: $dir not found."
-    exit 1
+if [ ! -f "$mksquashfs_bin" ]; then
+    echo "ERROR: mksquashfs not found."; exit 1
+elif [ ! -d "$dir" ]; then
+    echo "ERROR: $dir not found."; exit 2
 fi
+rm -r squashfs-init squashfs-root $oinit.sqfs.md5 $ofile.sqfs.md5 $ofile.pak $ofile.epk $ofile.zip
+#rm -r $oinit.sqfs $ofile.sqfs
 
-rm -r squashfs-root
+cp -r --preserve=timestamps trunk/lginit squashfs-init
+find squashfs-init -name '.svn' | xargs rm -rf
+for i in squashfs-init/lginit; do
+	sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" $i; done
+$mksquashfs_bin squashfs-init $oinit.sqfs -le -all-root -noappend -b 524288
+
 cp -r --preserve=timestamps $dir squashfs-root
+find squashfs-root -name '.svn' | xargs rm -rf
 cd squashfs-root
-tar xzf dev.tar.gz
-tar xzf dev-lgmod.tar.gz
-tar xzvf etc_passwd.tar.gz
+for i in dev.tar.gz dev-lgmod.tar.gz etc_passwd.tar.gz; do tar xzf $i; done
 rm -f dev.tar.gz dev-lgmod.tar.gz etc_passwd.tar.gz
-find . -name '.svn' | xargs rm -rf
+echo $LGMOD_VERSION > var/www/cgi-bin/version
+for i in etc/init.d/rcS etc/init.d/lgmod var/www/cgi-bin/footer.inc var/www/cgi-bin/header.inc; do
+	sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" $i; done
 cd ..
-
-echo $LGMOD_VERSION > ./squashfs-root/home/lgmod/www/cgi-bin/version
-sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" ./squashfs-root/home/lgmod/www/cgi-bin/footer.inc
-sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" ./squashfs-root/home/lgmod/www/cgi-bin/header.inc
-sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" ./squashfs-root/etc/lgmod.sh
-sed -i -e "s/ver=/S7 $LGMOD_VERSION/g" ./squashfs-root/etc/init.d/lgmod
-
-ofile=lgmod_S7_$LGMOD_VERSION_ROOTFS
-rm -f $ofile.pak $ofile.sqfs $ofile.epk $ofile.zip
-
 $mksquashfs_bin squashfs-root $ofile.sqfs -le -all-root -noappend -b 1048576
-md5sum $ofile.sqfs > $ofile.sqfs.md5
+
 osize=`wc -c $ofile.sqfs | awk '{print $1}'`
 echo "Size: $osize / $size"
 o4096=$(( $osize / 4096 * 4096 ))
-
-if [ "$osize" -gt "$size" ]
-then
+if [ "$osize" -gt "$size" ]; then
     echo "ERROR: Partition image too big for flashing ($osize / $size)."
-    rm -f $ofile.sqfs
-    rm -rf squashfs-root
-    exit 1
-elif [ "$osize" != "$o4096" ]
-then
+    rm -rf squashfs-root $ofile.sqfs
+    exit 3
+elif [ "$osize" != "$o4096" ]; then
     echo "ERROR: Partition image is not multiple of 4096 ($osize / $o4096)."
-    rm -f $ofile.sqfs
-    rm -rf squashfs-root
+    rm -rf squashfs-root $ofile.sqfs
     exit 4
-elif [ "$1" == 'pak' ] || [ "$1" == 'epk' ]
-then
+elif [ "$1" == 'pak' ] || [ "$1" == 'epk' ]; then
     $mkepk_bin -c $ofile.pak $ofile.sqfs root DVB-SATURN 0x$LGMOD_VERSION_ROOTFS `date +%Y%m%d` RELEASE
     $mkepk_bin -m 0x$LGMOD_VERSION_EPK HE_DTV_GP2M_AAAAABAA $ofile.epk $ofile.pak
     zip -j $ofile.zip $ofile.pak $ofile.epk
-    rm -f $ofile.pak
-    mv $ofile.epk ../
+    #mv $ofile.epk ../
+    rm -f $ofile.pak $ofile.epk
 fi
 
-oinit=mtd4_lg-init
-#rm -r squashfs-init
-#cp -r --preserve=timestamps trunk/lginit squashfs-init
-#find squashfs-init -name '.svn' | xargs rm -rf
-#$mksquashfs_bin squashfs-init $oinit.sqfs -le -all-root -noappend
-
-md5sum $oinit.sqfs > $oinit.sqfs.md5
-zip -j $ofile.zip $ofile.sqfs.md5 $ofile.sqfs $oinit.sqfs $oinit.sqfs.md5 changelog.txt install.sh
-rm -rf $ofile.sqfs.md5 $ofile.sqfs squashfs-root $oinit.sqfs.md5
-
+md5sum $oinit.sqfs > $oinit.sqfs.md5; md5sum $ofile.sqfs > $ofile.sqfs.md5
+zip -j $ofile.zip $oinit.sqfs $oinit.sqfs.md5 $ofile.sqfs $ofile.sqfs.md5 changelog.txt install.sh
 cat extract.sh $ofile.zip > $ofile.sh.zip
+rm -rf squashfs-root squashfs-init $oinit.sqfs.md5 $ofile.sqfs.md5; #$oinit.sqfs $ofile.sqfs
 #mv $ofile.zip ../
-rm -rf $ofile.zip; #$oinit.sqfs squashfs-init
+rm -rf $ofile.zip
--- lgmod_s7/install.sh
+++ lgmod_s7/install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# OpenLGTV BCM installation script v.1.5 by xeros
 # Source code released under GPL License
-# Rewriten for lgmod S7 by mmm4m5m
+# OpenLGTV BCM installation script v.1.5 by xeros
+# Rewriten for Saturn7 by mmm4m5m
 
 # config
-CHR=/tmp/install-root
-USB=/tmp/install-usb
+CHR=/tmp/install-root; USB=/tmp/install-usb; INS=/home/lgmod/install.sh
 DIR="${0%/*}"
 rootfs=$(echo "$DIR/"lgmod_S7_*.sqfs)
 MNT='/tmp /mnt/lg/lgapp /mnt/lg/lginit /mnt/lg/user /mnt/lg/cmn_data'
@@ -16,18 +15,37 @@ for i in "$@"; do
 	[ "$i" = info ]   && info=1;   [ "$i" = noinfo ]   && info=''
 	[ "$i" = chroot ] && chroot=1; [ "$i" = nochroot ] && chroot=''; done
 
-err=0
+if [ "$1" = steps ]; then
+	INIT=/mnt/lg/user/lgmod/init; USB=/tmp/lgmod/chrusb
+	usb="$USB${rootfs#/mnt/usb?/Drive?}"; usb="${usb%/*}"
+	echo "#
+# 0. Steps - start this script using absolute path to script file
+# 1. Prepare - replace 'sd?' with the actual device name
+mount | grep /dev/sd && mkdir -p $INIT
+echo 'LGI_MENU=1' >> $INIT/lginit
+echo 'LGI_CHROOT=sd?${rootfs#/mnt/usb?/Drive?}' >> $INIT/lginit
+# 2. Install - lginit image only and then reboot (restart TV)
+install.sh lginiton <ARGUMENTS> && reboot
+# 3. Check - wait few sec. Change options and reboot
+echo 'RCS_NOREL=1' >> $INIT/rcS && reboot
+# 4. Install - rootfs
+cd $usb; $INS <ARGUMENTS>
+# 5. Cleanup - do not forget
+rm -r $INIT
+#	"
+fi
 
 
 # info
 if [ -n "$info" ]; then
-	( err=0; echo -ne '\n\n#$# INFO: '; date
+	err=0
+	{ echo -ne "\n\n#$# INFO($err): "; date
 		echo -e '\n\n$#' df -h; df -h | grep -v '^/dev/sd' || err=12
 		echo -e '\n\n$#' busybox; busybox || err=12
 		echo -e '\n\n$#' help; help || err=10
 		echo -e '\n\n$#' dmesg; dmesg|grep ACTIVE;echo; dmesg || err=15
 		for i in /etc/version_for_lg /mnt/lg/model/* /mnt/lg/cmn_data/*exc_log_[0-9]*.txt /lgsw/* \
-			/etc/init.d/* /etc/rc.d/* /mnt/lg/user/lgmod/init/*; do [ ! -f "$i" ] && continue
+			/etc/init.d/rcS /etc/rc.d/rc.* /mnt/lg/user/lgmod/init/*; do [ ! -f "$i" ] && continue
 			echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 			echo -e '\n\n$#' cat "$i"; cat "$i" || err=16
 		done; echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -35,40 +53,47 @@ if [ -n "$info" ]; then
 		ls -lR /etc /mnt/lg/lginit /mnt/lg/user /mnt/lg/model /mnt/lg/lgapp /usr/local /mnt/lg/bt /mnt/lg/cmn_data /mnt/lg/res/lgres /mnt/lg/res/lgfont
 		ls -l /mnt/lg/ciplus/cert | sed -e 's/ [^ ]*-/ ...-/'; # private info?
 		ls -l /mnt/lg/ciplus /mnt/lg/ciplus/[^c]* /home/lgmod /mnt/lg/res/estreamer /mnt/addon /mnt/addon/* /mnt/addon/*/* /mnt/addon/*/*/*
-	exit $err ) >> /tmp/install-info || err=$?
-	[ $err != 0 ] && echo "Error($err): Info file failed: $infofile"; err=0
+	} >> /tmp/install-info
+	[ $err != 0 ] && echo "Error($err): Info file failed: $infofile"
 fi
+
+err=0
 
 
 INFO="$DIR"; ip=''; [ -f /mnt/lg/user/lgmod/ftp ] && ! tty > /dev/null &&
 	ip=$(ifconfig eth0|grep addr|sed -e'2!d' -e's/^[^:]*://' -e's/ .*//'|grep -v 255 2>/dev/null)
 [ -n "$ip" ] && INFO="<a href='ftp://$ip/${INFO#$(cat /mnt/lg/user/lgmod/ftp)}'>$INFO</a>"
 
+mkdir -p "$CHR"; mount -t squashfs "$rootfs" "$CHR"
+[ -f "$CHR$INS" ] || { err=7; echo "Error($err): File not found: $CHR$INS"; }
 
-mkdir -p "$CHR" && mount -t squashfs "$rootfs" "$CHR" || err=5
-[ $err = 0 ] || { echo "Error($err): Mount root failed: $rootfs ($CHR)"; exit $err; }
+
+[ $err != 0 ] && exit $err
 
 if [ -n "$chroot" ]; then
-	mount -t proc  installproc  "$CHR/proc" || err=6
-	mount -t sysfs installsysfs "$CHR/sys" || err=6
-	mount -t usbfs installusbfs "$CHR/proc/bus/usb" || err=6
-	for i in $MNT; do mount -o bind "$i" "$CHR$i" || err=7; done
-	mkdir -p "$USB" && mount -o bind "$DIR" "$CHR$USB" || err=8
+	mount -t sysfs installsysfs "$CHR/sys" &&
+		mount -t proc  installproc  "$CHR/proc" &&
+		mount -t usbfs installusbfs "$CHR/proc/bus/usb"
+	if [ -d "$CHR/proc/bus/usb" ]; then
+		for i in $MNT "$USB"; do mount -o bind "$i" "$CHR$i"; done
+		mkdir -p "$CHR$USB"; mount -o bind "$USB" "$CHR$USB"
 
-	if [ $err != 0 ]; then echo "Error($err): Chroot failed."
-	else
-		echo "Output directory: $INFO"; echo
-		chroot "$CHR" /home/lgmod/install-chroot.sh "$@" "workdir=$USB" busybox=/bin/busybox ||
-			{ ERR=$?; [ $ERR -gt 99 ] && exit $ERR; }; # keep chroot environment
-	fi
+		if [ -d "$CHR$USB" ]; then
+			echo "Output directory: $INFO"; echo
+			chroot "$CHR" "$INS" "$@" "workdir=$USB" busybox=/bin/busybox ||
+				{ err=$?; [ $err -gt 99 ] && exit $err; }; # keep chroot environment
+		else err=8; echo "Error($err): Chroot failed: $CHR$USB"; fi
 
-	umount "$CHR/sys" "$CHR/proc" "$CHR$USB" && rm -r "$CHR$USB"
-	for i in $MNT; do umount "$CHR$i"; done
+		umount "$CHR$USB" && rm -r "$CHR$USB"
+		for i in $MNT; do umount "$CHR$i"; done
+	else err=9; echo "Error($err): Chroot failed: $CHR/proc/bus/usb"; fi
+	umount "$CHR/proc/bus/usb" "$CHR/proc" "$CHR/sys"
 else
 	echo "Output directory: $INFO"; echo
-	cd "$DIR"; "$CHR/home/lgmod/install-chroot.sh" "$@" ||
-		{ ERR=$?; [ $ERR -gt 99 ] && exit $ERR; }; # keep root mounted
+	cd "$DIR"; "$CHR$INS" "$@" ||
+		{ err=$?; [ $err -gt 99 ] && exit $err; }; # keep root mounted
 fi
+
 
 umount "$CHR" && rm -r "$CHR"
 exit $err
